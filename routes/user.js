bac5acda03afa0a2bb68bedaf29934c9d56a3f44
deepaks225 <deepaksingh271201@gmail.com>
2024-09-28 15:56:28 +0530

    user.js

--- routes/user.js
+++ routes/user.js
@@ -8,8 +8,13 @@ const User = require('../models/user.js');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-  
 
+cloudinary.config({
+    cloud_name: 'dbhbtrccn',
+    api_key: '466272138665527',
+    api_secret: 'B6UEwglvi3jT5LRCF04lSOzxfgs'
+ });  
+  
 // Set up Cloudinary storage for profile pictures and report images
 const profileStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
